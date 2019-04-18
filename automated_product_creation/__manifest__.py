6343068b22b1f791ea882f59ed00950653359dd9
Aswanth Babu S <49155214+aswanth7232@users.noreply.github.com>
2019-04-18 16:19:46 +0530

    Update __manifest__.py

--- automated_product_creation/__manifest__.py
+++ automated_product_creation/__manifest__.py
@@ -1,23 +1,30 @@
-# -*- coding: utf-8 -*-
-{
-    'name': "Automated Product Creation",
 
+# -*- coding: utf-8 -*-
+##############################################################################      
+#         _______  _____            _______  _       _______
+#         |     | |       |       | |     | | \    |    |    |     |
+#         |_____| |_____  |   _   | |_____| |  \   |    |    |_____|
+#         |     |       | |  / \  | |     | |   \  |    |    |     |
+#         |     |  _____| |_/   \_| |     | |    \_|    |    |     |
+#                 
+##############################################################################
+
+{
+    
+    'name': 'Automated Product Creation',
     'summary': 'This module help to  create product using CRON',
     'author': 'Aswanth',
-    'category': 'Tools',
     'version': '12.0.1.1.0',
-
+    'category': 'Tools',
     'depends': ['base','product'],
-
-    
-    'data': [
+     'data': [
        
         'data/scheduler_action.xml',
     ],
-    'images': ['static/description/icon.png'],
+   	'images': ['static/description/icon.png'],
+   	'license': 'AGPL-3',
     'installable': True,
-    'license': 'AGPL-3',
     'application': True,
     'auto_install': False,
-   
 }
+
